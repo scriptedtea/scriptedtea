23b7edee85944d12d2b16fa3425d87e8fec8847b
Lily Guo <lily2guo@gmail.com>
2019-02-10 00:35:20 -0800

    Edited Paginator, tried to configure comments but failed, extracted related posts but still doesn't work

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,17 +1,17 @@
 var store = [{
         "title": "Benefits of static site generators",
         "excerpt":"Some of the benefits of a static website generators are that they are: Simple - makes building and maintaining websites incredibly simple, Static sites are easier to maintian (No databases, no server-side scripts!). User static website generators when you’re after simplicity and want to put the emphasis on the content,...","categories": ["tech"],
-        "tags": [],
+        "tags": ["fun"],
         "url": "https://scriptedtea.github.io/scriptedtea/tech/benefits-of-static-website-generators/",
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/kyoto1.jpg"},{
         "title": "Learning Jekyll",
         "excerpt":"","categories": ["tech"],
-        "tags": [],
+        "tags": ["fun"],
         "url": "https://scriptedtea.github.io/scriptedtea/tech/learning-jekyll/",
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/kyoto1.jpg"},{
         "title": "Blah",
         "excerpt":"blahblahblahblahblahlblahblahb lahblahblahlblahblahblahb lahblahlblahblahblahblahblahlbl ahblahblahblahblahlbl ahblahblahblahblah lblahblahblahblahblahlblahb lahblahblahblahlblahblahblahblah blahlblahblahblahbl ahblahlblahblahblahbla hblahlblahblahblahbla hblahlblahblahblahbl ahblahlblahblahblahblahblahl blahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahl blahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahl ","categories": ["miscellaneous"],
-        "tags": [],
+        "tags": ["diary"],
         "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/blah/",
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/kyoto1.jpg"},{
         "title": "Happy New Year!",
