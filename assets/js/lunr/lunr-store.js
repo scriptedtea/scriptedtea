b9a7cebc0770c2c9756a2ee4a3ec509d8c8a7732
Lily Guo <lily2guo@gmail.com>
2019-02-18 12:14:51 -0800

    Adjusted width and fixed any bugs that came with it

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -15,7 +15,7 @@ var store = [{
         "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/blah/",
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/kyoto1.jpg"},{
         "title": "Happy New Year!",
-        "excerpt":"Happy New Year! I was on a plane when the clock passed midnight while coming back from my vacation in China. My 2-week trip went by in a blur, and shortly I was back to work. Unfortunately I caught a stomach flu during my trip coming back so I took...","categories": ["miscellaneous"],
+        "excerpt":"Happy New Year! I was on a plane when the clock striked past midnight while coming back on my trip from China. My 2-week vacation went by in a blur, and the next thing I knew I was already back to work. Unfortunately I caught a stomach flu nearing the...","categories": ["miscellaneous"],
         "tags": [],
         "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/happy-new-year/",
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/kyoto1.jpg"},]
