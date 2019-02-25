af08d34bce5cdf8609aebd6b50180a15568b2c67
Lily Guo <lily2guo@gmail.com>
2019-02-24 23:31:51 -0800

    Add in support for related posts based on category and number of similar tags

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -11,11 +11,11 @@ var store = [{
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/generated/kyoto1-1000by664-43c81d.jpg"},{
         "title": "Blah",
         "excerpt":"blahblahblahblahblahlblahblahb lahblahblahlblahblahblahb lahblahlblahblahblahblahblahlbl ahblahblahblahblahlbl ahblahblahblahblah lblahblahblahblahblahlblahb lahblahblahblahlblahblahblahblah blahlblahblahblahbl ahblahlblahblahblahbla hblahlblahblahblahbla hblahlblahblahblahbl ahblahlblahblahblahblahblahl blahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahl blahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahl ","categories": ["miscellaneous"],
-        "tags": ["diary"],
+        "tags": ["diary","fun"],
         "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/blah/",
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/generated/kyoto1-1000by664-43c81d.jpg"},{
         "title": "Happy New Year!",
         "excerpt":"Happy New Year! I was on a plane when the clock striked past midnight while coming back on my trip from China. My 2-week vacation went by in a blur, and the next thing I knew I was already back to work. Unfortunately I caught a stomach flu nearing the...","categories": ["miscellaneous"],
-        "tags": [],
+        "tags": ["diary"],
         "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/happy-new-year/",
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/generated/kyoto1-1000by664-43c81d.jpg"},]
