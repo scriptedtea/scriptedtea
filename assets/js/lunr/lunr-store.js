907173a1f2516122ff1bf631b5a27bbedb0acb61
Lily Guo <lily2guo@gmail.com>
2019-02-25 00:00:44 -0800

    Fix so that grid_wrapper is outside of the for loop, max limit related posts to 3

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,13 @@
 var store = [{
         "title": "Benefits of static site generators",
-        "excerpt":"Some of the benefits of a static website generators are that they are: Simple - makes building and maintaining websites incredibly simple, Static sites are easier to maintian (No databases, no server-side scripts!). User static website generators when you’re after simplicity and want to put the emphasis on the content,...","categories": ["tech"],
-        "tags": ["fun"],
-        "url": "https://scriptedtea.github.io/scriptedtea/tech/benefits-of-static-website-generators/",
+        "excerpt":"Some of the benefits of a static website generators are that they are: Simple - makes building and maintaining websites incredibly simple, Static sites are easier to maintian (No databases, no server-side scripts!). User static website generators when you’re after simplicity and want to put the emphasis on the content,...","categories": ["miscellaneous","tech"],
+        "tags": ["diary","fun"],
+        "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/tech/benefits-of-static-website-generators/",
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/generated/kyoto1-1000by664-43c81d.jpg"},{
         "title": "Learning Jekyll",
-        "excerpt":"","categories": ["tech"],
-        "tags": ["fun"],
-        "url": "https://scriptedtea.github.io/scriptedtea/tech/learning-jekyll/",
+        "excerpt":"","categories": ["miscellaneous","tech"],
+        "tags": ["diary","fun"],
+        "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/tech/learning-jekyll/",
         "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/generated/kyoto1-1000by664-43c81d.jpg"},{
         "title": "Blah",
         "excerpt":"blahblahblahblahblahlblahblahb lahblahblahlblahblahblahb lahblahlblahblahblahblahblahlbl ahblahblahblahblahlbl ahblahblahblahblah lblahblahblahblahblahlblahb lahblahblahblahlblahblahblahblah blahlblahblahblahbl ahblahlblahblahblahbla hblahlblahblahblahbla hblahlblahblahblahbl ahblahlblahblahblahblahblahl blahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahl blahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahl ","categories": ["miscellaneous"],
