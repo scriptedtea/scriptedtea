c696db356fc3bd34834b8020ce8347842b124e8e
Lily Guo <lily2guo@gmail.com>
2019-04-07 11:26:03 -0700

    Added detailed project page, fixed project feed not aligned to the centre

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -104,3 +104,13 @@ $(document).ready(function() {
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 });
+
+// Magnific-Popup for custom HTMl
+$('.open-popup-html').magnificPopup({
+  type: 'ajax',
+  midClick: true,
+  closeBtnInside: true,
+  showCloseBtn: true,
+  closeOnBgClick: true
+});
+
