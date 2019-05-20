ee4e06733344fd4ac582f1388ef402f620ba0e80
Lily Guo <lily2guo@gmail.com>
2019-05-20 11:34:10 -0700

    Fixed post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Happy New Year!",
-        "excerpt":"Happy New Year! I was on a plane when the clock striked past midnight while coming back on my trip from China. My 2-week vacation went by in a blur, and the next thing I knew I was already back to work. Unfortunately I caught a stomach flu nearing the...","categories": ["miscellaneous"],
+        "excerpt":"Happy New Year! I was on a plane when the clock striked past midnight while coming back on my trip from China. My 2 week vacation went by in a blur, and the next thing I knew I was already back to work. Unfortunately I caught a flu nearing the...","categories": ["miscellaneous"],
         "tags": ["diary"],
         "url": "https://scriptedtea.com/miscellaneous/happy-new-year/",
         "teaser":"https://scriptedtea.com/assets/images/posts/image6-600by602-414ee3.jpg"},]
