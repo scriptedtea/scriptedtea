a3767f536826bd0f1da4ad49b0a64050fa7b5e8f
Lily Guo <lily2guo@gmail.com>
2019-05-19 21:44:00 -0700

    Changed first page to list view instead of grid view for recent posts, polished first blog post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,21 +1,6 @@
 var store = [{
-        "title": "Benefits of static site generators",
-        "excerpt":"Some of the benefits of a static website generators are that they are: Simple - makes building and maintaining websites incredibly simple, Static sites are easier to maintian (No databases, no server-side scripts!). User static website generators when you’re after simplicity and want to put the emphasis on the content,...","categories": ["miscellaneous","tech"],
-        "tags": ["diary","fun"],
-        "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/tech/benefits-of-static-website-generators/",
-        "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/generated/kyoto1-1000by664-43c81d.jpg"},{
-        "title": "Learning Jekyll",
-        "excerpt":"","categories": ["miscellaneous","tech"],
-        "tags": ["diary","fun"],
-        "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/tech/learning-jekyll/",
-        "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/generated/kyoto1-1000by664-43c81d.jpg"},{
-        "title": "Blah",
-        "excerpt":"blahblahblahblahblahlblahblahb lahblahblahlblahblahblahb lahblahlblahblahblahblahblahlbl ahblahblahblahblahlbl ahblahblahblahblah lblahblahblahblahblahlblahb lahblahblahblahlblahblahblahblah blahlblahblahblahbl ahblahlblahblahblahbla hblahlblahblahblahbla hblahlblahblahblahbl ahblahlblahblahblahblahblahl blahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahl blahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahlblahblahblahblahblahl ","categories": ["miscellaneous"],
-        "tags": ["diary","fun"],
-        "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/blah/",
-        "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/generated/kyoto1-1000by664-43c81d.jpg"},{
         "title": "Happy New Year!",
         "excerpt":"Happy New Year! I was on a plane when the clock striked past midnight while coming back on my trip from China. My 2-week vacation went by in a blur, and the next thing I knew I was already back to work. Unfortunately I caught a stomach flu nearing the...","categories": ["miscellaneous"],
         "tags": ["diary"],
         "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/happy-new-year/",
-        "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/generated/kyoto1-1000by664-43c81d.jpg"},]
+        "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/posts/image6-600by602-414ee3.jpg"},]
