849c29b38dc8fe1a25f849e52d465aa209e090a5
Lily Guo <lily2guo@gmail.com>
2019-05-20 16:21:27 -0700

    Added new post, tweaked about page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Happy New Year! I was on a plane when the clock striked past midnight while coming back on my trip from China. My 2 week vacation went by in a blur, and the next thing I knew I was already back to work. Unfortunately I caught a flu nearing the...","categories": ["miscellaneous"],
         "tags": ["diary"],
         "url": "https://scriptedtea.com/miscellaneous/happy-new-year/",
-        "teaser":"https://scriptedtea.com/assets/images/posts/image6-600by602-414ee3.jpg"},]
+        "teaser":"https://scriptedtea.com/assets/images/posts/image6-600by602-414ee3.jpg"},{
+        "title": "Why I Chose Jekyll to Build My Website",
+        "excerpt":"I’ve been wanting to create my own website for a long time, but never really committed to making one until recently. Since I had no former experience with web development, I had to start from the very beginning - from researching which stack to use all the way to web...","categories": ["tech"],
+        "tags": ["jekyll","web development"],
+        "url": "https://scriptedtea.com/tech/why-jekyll/",
+        "teaser":"https://scriptedtea.com/assets/images/posts/jekyll_logo.png"},]
