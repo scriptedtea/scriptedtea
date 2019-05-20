6d9148e49cc8b167501e9af10268d673ac397277
Lily Guo <lily2guo@gmail.com>
2019-05-20 11:26:58 -0700

    Edit config yml to the custom domain

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Happy New Year!",
         "excerpt":"Happy New Year! I was on a plane when the clock striked past midnight while coming back on my trip from China. My 2-week vacation went by in a blur, and the next thing I knew I was already back to work. Unfortunately I caught a stomach flu nearing the...","categories": ["miscellaneous"],
         "tags": ["diary"],
-        "url": "https://scriptedtea.github.io/scriptedtea/miscellaneous/happy-new-year/",
-        "teaser":"https://scriptedtea.github.io/scriptedtea/assets/images/posts/image6-600by602-414ee3.jpg"},]
+        "url": "https://scriptedtea.com/miscellaneous/happy-new-year/",
+        "teaser":"https://scriptedtea.com/assets/images/posts/image6-600by602-414ee3.jpg"},]
