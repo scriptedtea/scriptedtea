25c5efb50689b816f21829928a105689260f61b9
Lily Guo <lily2guo@gmail.com>
2020-05-03 15:23:44 -0700

    Push after a build

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,25 +2,25 @@ var store = [{
         "title": "Happy New Year!",
         "excerpt":"Happy New Year! I was on a plane when the clock striked past midnight while coming back on my trip from China. My 2 week vacation went by in a blur, and the next thing I knew I was already back to work. Unfortunately I caught a flu nearing the...","categories": ["miscellaneous"],
         "tags": ["diary"],
-        "url": "http://localhost:4000/miscellaneous/happy-new-year/",
-        "teaser":"http://localhost:4000/assets/images/posts/image6-600by602-414ee3.jpg"},{
+        "url": "https://scriptedtea.com/miscellaneous/happy-new-year/",
+        "teaser":"https://scriptedtea.com/assets/images/posts/image6-600by602-414ee3.jpg"},{
         "title": "Why I Chose Jekyll to Build My Website",
         "excerpt":"I’ve been wanting to create my own website for a long time, but never really committed to making one until recently. Since I had no former experience with web development, I had to start from the very beginning - from researching which stack to use all the way to web...","categories": ["tech"],
         "tags": ["jekyll","github pages","web development"],
-        "url": "http://localhost:4000/tech/why-jekyll/",
-        "teaser":"http://localhost:4000/assets/images/posts/jekyll_logo.png"},{
+        "url": "https://scriptedtea.com/tech/why-jekyll/",
+        "teaser":"https://scriptedtea.com/assets/images/posts/jekyll_logo.png"},{
         "title": "How to Build a Static Jekyll Site with Github Pages",
         "excerpt":"Image Credit to Lars Veelaert One of the great things about Jekyll is that it has close integration with Github Pages, which means you can let Github host and publish your website with just a few config changes. There are a lot of guides (for example, the official Github documentation)...","categories": ["tech"],
         "tags": ["jekyll","github pages","web development"],
-        "url": "http://localhost:4000/tech/deployment/",
-        "teaser":"http://localhost:4000/assets/images/posts/jekyll_pages.jpg"},{
+        "url": "https://scriptedtea.com/tech/deployment/",
+        "teaser":"https://scriptedtea.com/assets/images/posts/jekyll_pages.jpg"},{
         "title": "Flutter and the Mobile World",
         "excerpt":"I’ve always been wanting to try doing mobile, but never got a chance to work on it until now. One of the reasons why I bought a Macbook back in 2013 was just so I could do iOS development, but I somehow never got around to do that. Other than...","categories": ["tech"],
         "tags": ["flutter","android","ios","dart","mobile"],
-        "url": "http://localhost:4000/tech/flutter-intro/",
-        "teaser":"http://localhost:4000/assets/images/posts/flutter_logo.png"},{
+        "url": "https://scriptedtea.com/tech/flutter-intro/",
+        "teaser":"https://scriptedtea.com/assets/images/posts/flutter_logo.png"},{
         "title": "How to Stream Computer Audio and Microphone on Mac Using Blackhole",
         "excerpt":"Since the COVID-19 lockdown started, one of the things I do to stay connected with friends is by playing games virtually. One of the most popular ones, which I also own, is Jackbox Games. We would all join a video conference call (eg. Google Meet or Zoom) together, I would...","categories": ["tech"],
         "tags": ["how-to","mac","audio"],
-        "url": "http://localhost:4000/tech/how-to-stream-audio-using-blackhole/",
-        "teaser":"http://localhost:4000/assets/images/posts/2020-05-03/zoom_hangouts-600-0d6564.jpg"},]
+        "url": "https://scriptedtea.com/tech/how-to-stream-audio-using-blackhole/",
+        "teaser":"https://scriptedtea.com/assets/images/posts/2020-05-03/zoom_hangouts-600-0d6564.jpg"},]
